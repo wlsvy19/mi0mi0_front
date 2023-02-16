283689ae63dce4841195b332c738bcb5a2f94b79
leejp <jinpyolee19@naver.com>
2023-02-16 16:21:30 +0900

    project setting

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>기본 페이지</h1>
+    <h1>{{ testMethod() }}</h1>
+    <QnABoard></QnABoard>
+  </div>
+</template>
+
+<script>
+import QnABoard from '~/components/QnABoard.vue';
+export default {
+  comments: {
+    QnABoard
+  },
+  methods: {
+    testMethod() {
+      return "hello hello";
+    }
+  },
+};
+</script>
